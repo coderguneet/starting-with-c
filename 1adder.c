9038c40d338b919b8c901416946907a807b75472
Guneet Singh <coderguneet@gmail.com>
2024-12-11 11:46:50 +0530

    added the code file

---
+++ 1adder.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers with a space seperating them: ");
+    scanf("%d %d", &a, &b);
+    printf("Sum: %d\n", a + b);
+    return 0;
+}
